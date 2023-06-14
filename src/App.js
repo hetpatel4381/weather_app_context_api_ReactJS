e947594aa40dbc42687cb94870ae05589279eb9d
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-06-15 00:38:50 +0530

    completed the weather git project using context api in React Js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import Card from "./components/Card";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import { useWeather } from "./context/Weather";
+
+import "./App.css";
 
 function App() {
+  const weather = useWeather();
+  console.log(weather);
+
+  useEffect(() => {
+    // Get Current Location.
+    weather.fetchCurrentUserLocationData();
+  }, [weather]); // Add 'weather' as a dependency
+
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
+      <Input />
+      <Button onClick={weather.fetchData} value="Search" />
+      <Card />
+      <Button value="Refresh" />
     </div>
   );
 }
---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+const baseURL =
+  "https://api.weatherapi.com/v1/current.json?key=60a12b03140941a6b44172823231406";
+
+export const getWeatherDataForCity = async (city) => {
+  const response = await fetch(`${baseURL}&q=${city}&aqi=yes`);
+  return await response.json();
+};
+
+export const getWeatherDataForLocation = async (lat, lon) => {
+  const response = await fetch(`${baseURL}&q=${lat},${lon}&aqi=yes`);
+  return await response.json();
+};
